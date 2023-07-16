9bec0eabe3f79a908ecaf5943e1573c7fa64f6b9
emir <me.bouhamar@esi-sba.dz>
2023-07-16 15:23:38 +0100

    pirsma init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+const RootPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return <div className=""></div>;
+};
+export default RootPage;
